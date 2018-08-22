73adc61cfc0ac2f9833bd4af27bd0e6ae156f94d
Cyrus Goh <hello@lovincyrus.com>
2018-08-22 10:43:24 +0800

    Refactor routing with '@reach/router'

--- src/App.js
+++ src/App.js
@@ -1,47 +1,14 @@
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from 'react-router-dom';
-import Company from './components/Company.js';
-import Category from './components/Category.js';
+import React, { Component } from 'react';
+import { Router, Link } from '@reach/router'
 
-class ModalSwitch extends React.Component {
-  componentWillUpdate(nextProps) {
-    const { location } = this.props;
+import Company from './components/Company.js'
 
-    if (
-      nextProps.history.action !== 'POP' &&
-      (!location.state || !location.state.modal)
-    ) {
-      this.previousLocation = this.props.location;
-    }
-  }
-
-  previousLocation = this.props.location;
-
+export default class App extends Component {
   render() {
-    const { location } = this.props;
-    const isModal = !!(
-      location.state &&
-      location.state.modal &&
-      this.previousLocation !== location
-    );
     return (
-      <div>
-        <Switch location={isModal ? this.previousLocation : location}>
-          <Route exact path="/" component={Company} />
-        </Switch>
-      </div>
-    );
+      <Router>
+        <Company path="/" />
+      </Router>
+    )
   }
 }
-
-const App = () => (
-  <Router>
-    <Route component={ModalSwitch} />
-  </Router>
-);
-
-export default App;
