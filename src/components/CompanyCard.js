063600547121322ab75e67ed9f664806e29f5c0a
Cyrus Goh <hello@lovincyrus.com>
2018-09-25 16:42:47 -0700

    Add fontawesome

--- src/components/CompanyCard.js
+++ src/components/CompanyCard.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import Helmet from 'react-helmet';
-import { Container, Flex, Image, Link } from '@hackclub/design-system';
+import { Container, Flex, Image, Link, Text } from '@hackclub/design-system';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faLink } from '@fortawesome/free-solid-svg-icons';
 
 import users from '../data.json';
 import meta from '../meta.json';
 
 import '../App.css';
+
+library.add(faEnvelope);
+library.add(faLink);
 
 const { description, url, title, name, favicon } = meta;
 class CompanyCard extends React.Component {
@@ -15,7 +21,11 @@ class CompanyCard extends React.Component {
     const filterCompanyName = users.filter(item => item.company === company);
 
     const pStyle = {
-      marginBottom: '8px'
+      marginTop: '10px',
+      marginBottom: '8px',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center'
     };
 
     const aWebsiteStyle = {
@@ -27,8 +37,19 @@ class CompanyCard extends React.Component {
       borderRadius: '4px',
       backgroundColor: '#5F84FF',
       color: 'white',
-      width: '100px',
+      width: '117px',
       transition: 'all 0.3s ease 0s'
+    };
+
+    const faIconStyle = {
+      display: 'block',
+      marginBlockStart: '1em',
+      marginBlockEnd: '1em',
+      marginInlineStart: '0px',
+      marginInlineEnd: '0px',
+      right: '11px',
+      top: '-6px',
+      position: 'absolute'
     };
 
     return (
@@ -42,7 +63,7 @@ class CompanyCard extends React.Component {
           <link rel="canonical" href={url} />
           <link rel="shortcut icon" href={favicon} />
         </Helmet>
-        <div className="Brothers">
+        <React.Fragment>
           <Flex mx={[1, 2, -3]} wrap justify="center">
             <h1 style={{ fontSize: '2.5em' }}>{company}</h1>
           </Flex>
@@ -57,13 +78,16 @@ class CompanyCard extends React.Component {
                         alt="avatar"
                         style={{ width: '120px', height: '120px' }}
                       />
-                      <p style={pStyle}>{foo.name}</p>
+                      <Text style={pStyle}>{foo.name}</Text>
+                      <Link href={`mailto:${foo.email}`} color="slate">
+                        <FontAwesomeIcon icon="envelope" style={faIconStyle} />
+                      </Link>
                       <Link
                         href={foo.website}
                         target="_blank"
                         style={aWebsiteStyle}
                       >
-                        website
+                        <FontAwesomeIcon icon="link" /> website
                       </Link>
                     </Flex>
                   </Container>
@@ -71,7 +95,7 @@ class CompanyCard extends React.Component {
               </div>
             ))
           )}
-        </div>
+        </React.Fragment>
       </React.Fragment>
     );
   }
