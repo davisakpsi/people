e39c5afe13c0bbd7298a9b42649d83d25a3a9905
Cyrus Goh <hello@lovincyrus.com>
2018-09-30 10:09:40 -0700

    Add NoMatch in router

--- src/App.js
+++ src/App.js
@@ -3,12 +3,21 @@ import { Router } from '@reach/router';
 
 import Company from './components/Company.js';
 import CompanyCard from './components/CompanyCard.js';
+
+const NoMatch = ({ location }) => (
+  <div>
+    <h3>
+      Uh-oh! <code>{location.pathname}</code> is not found!
+    </h3>
+  </div>
+);
 export default class App extends Component {
   render() {
     return (
       <Router>
         <Company path="/" />
         <CompanyCard path="/:company" />
+        <NoMatch default />
       </Router>
     );
   }
